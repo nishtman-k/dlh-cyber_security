1c7a752013858cc16c4ae2352e96cfb63e6267b0
nishtman-k <n-kano@hotmail.com>
2026-05-11 19:35:56 +0200

    added find grouping with -o

--- linux_security/0x01_permissions_sguid_sgid/6-check_files.sh
+++ linux_security/0x01_permissions_sguid_sgid/6-check_files.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo find "$1" -perm -6000 -type f -mtime -1 -exec ls -l {} \; 2>/dev/null
+sudo find "$1" \( -perm -4000 -o -perm 2000 \) -type f -mtime -1 -exec ls -l {} \; 2>/dev/null
