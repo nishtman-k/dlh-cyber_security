7421bd7a470b6855d5dc373c3eeafe0a377db114
nishtman-k <n-kano@hotmail.com>
2026-09-07 14:01:13 +0200

    added 4-write_file script

---
+++ scripting_cyber/0x00-ruby_scripting/4-write_file.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+def merge_json_files(file1_path, file2_path)
+
+  file1_content = File.read(file1_path)
+  data1 = JSON.parse(file1_content)
+
+  file2_content = File.read(file2_path)
+  data2 = JSON.parse(file2_content)
+
+  merged_data = data2 + data1
+
+  File.write(file2_path, JSON.pretty_generate(merged_data))
+
+end
