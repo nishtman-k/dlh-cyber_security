12f9dd43d8615197689042bcc999bd1ab61be719
nishtman-k <n-kano@hotmail.com>
2026-09-07 14:27:52 +0200

    added 6-get script

---
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'uri'
+require 'json'
+
+def get_request(url)
+  # 1. Convert the string URL into a URI object
+  uri = URI.parse(url)
+
+  # 2. Perform the HTTP GET request
+  response = Net::HTTP.get_response(uri)
+
+  # 3. Print the status code and status message (e.g., "200 OK")
+  puts "Response status: #{response.code} #{response.message}"
+
+  # 4. Parse the body as JSON and pretty print it
+  parsed_body = JSON.parse(response.body)
+  puts "Response body:"
+  puts JSON.pretty_generate(parsed_body)
+end
