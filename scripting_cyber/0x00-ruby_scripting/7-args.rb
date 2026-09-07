d82b4d8853d41a9fe5a5b2775c519d43baef0dc1
nishtman-k <n-kano@hotmail.com>
2026-09-07 14:40:43 +0200

    added list output to 7-args script

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,16 +1,11 @@
 #!/usr/bin/env ruby
-
 def print_arguments
-  # 1. Check if the ARGV array is empty
   if ARGV.empty?
     puts "No arguments provided."
   else
-    # 2. Print the main header required by the checker
     puts "Arguments:"
-
-    # 3. Loop through and print each argument on a new line
-    ARGV.each do |arg|
-      puts arg
+    ARGV.each_with_index do |arg, i|
+      puts "#{i + 1}. #{arg}"
     end
   end
 end
